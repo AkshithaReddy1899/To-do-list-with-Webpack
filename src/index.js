94d5085b38d32934145fe5731ee77cce475eded2
Akshitha Reddy <akshithareddy1899@gmail.com>
2022-01-12 18:16:30 +0530

    Add material icon

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import taskArr from './module/task.js';
-import displayList from "./module/display.js";
+import icon from './module/icon.js';
 import css from "../src/style.css"
 
-console.log(taskArr)
+const list = document.getElementById('list');
+
+const displayList = (item) => {
+  list.innerHTML += `<li>
+  <input type="checkbox">
+  <p class="task-description">${item.description}</p>
+  <button type="button" class="li-btn">${icon}</button>
+  </li>`
+}
 
 taskArr.forEach((item) => {
-  displayList(item)
+  displayList(item);
 })
